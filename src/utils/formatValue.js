1e50f8dce125c6d5f0b8cd2a110257b8bad41107
Julien <newdevcreator@gmail.com>
2024-02-19 09:06:15 +0100

    FEAT: function progress bare

---
+++ src/utils/formatValue.js
@@ -0,0 +1,10 @@
+export default function formValue(val) {
+  const currentMin = Math.trunc(val / 60);
+  let currentSec = Math.trunc(val % 60);
+
+  if (currentSec < 10) {
+    currentSec = `0${currentSec}`;
+  }
+
+  return `${currentMin}:${currentSec}`;
+}
