d2b7f2509364467d701af53d8a499c3e2fb64439
Julien <newdevcreator@gmail.com>
2024-02-19 09:05:53 +0100

    FEAT: function progress bare

--- src/layouts/Player/Progress.jsx
+++ src/layouts/Player/Progress.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
+import formValue from "../../utils/formatValue";
+
 export default function Progress() {
   const progressData = useSelector((state) => state.progress);
-  console.log(progressData);
+  console.log(progressData.current / progressData.totalDuration);
+
+  function handleProgressClick(e) {
+    const player = document.getElementById("audio-player");
+    const rect = e.target.getBoundingClientRect();
+    const width = rect.width;
+    const x = e.clientX - rect.left;
+
+    player.currentTime = (x / width) * progressData.totalDuration;
+  }
 
   return (
     <div className="max-w-[800px] mx-auto">
-      <div className="bg-slate-900 h-2 rounded cursor-pointer">
-        <div className="bg-indigo-400 scale-x-50 origin-left h-full pointer-events-none"></div>
+      <div
+        onClick={handleProgressClick}
+        className="bg-slate-900 h-2 rounded cursor-pointer overflow-hidden"
+      >
+        <div
+          style={{
+            transform: `scaleX(${
+              progressData.current / progressData.totalDuration
+            })`,
+          }}
+          className="bg-indigo-400 origin-left h-full pointer-events-none"
+        ></div>
       </div>
       <div className="flex justify-between">
-        <span>2:15</span>
-        <span>9:15</span>
+        <span>{formValue(progressData.current)}</span>
+        <span>{formValue(progressData.totalDuration)}</span>
       </div>
     </div>
   );
